fc9595a4ed62c0115c6eba13808397d5fd337af9
d3v-g <janewang@wangjiayes-MacBook-Air.local>
2023-07-27 21:47:02 +0100

    feat: tailwind css setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
       <div>
+        <div className='text-red-600'>
+          Your theme is
+        </div>
+        <div className='text-lime-600'>
+          You are currently on the Home page
+        </div>
       </div>
     </>
   )
