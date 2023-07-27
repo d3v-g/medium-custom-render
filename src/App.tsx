f3e58f45fd67037adbae547f74556ee74b6206cd
d3v-g <janewang@wangjiayes-MacBook-Air.local>
2023-07-27 22:37:07 +0100

    chore: prettier formatting setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 function App() {
-
-  return (
-    <>
-      <div>
-        <div className='text-red-600'>
-          Your theme is
-        </div>
-        <div className='text-lime-600'>
-          You are currently on the Home page
-        </div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <div className="text-red-600">Your theme is</div>
+                <div className="text-lime-600">
+                    You are currently on the Home page
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
